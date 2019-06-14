cbe0488a47699ac0af4d59691f9b0f8d7ea8120d
Shushil Kumar <shushilkumarr@gmail.com>
2019-06-14 13:29:18 +0530

    initial commit

---
+++ app/src/main/java/com/example/firebasephoneauth/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.firebasephoneauth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextMobile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextMobile = findViewById(R.id.editTextMobile);
+
+        findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mobile = editTextMobile.getText().toString().trim();
+
+                if (mobile.isEmpty() || mobile.length() < 10) {
+                    Log.d("mobile no","invalid");
+                    editTextMobile.setError("Enter a valid mobile");
+                    editTextMobile.requestFocus();
+                    return;
+                }
+
+                Intent intent = new Intent(MainActivity.this, VerifyPhoneActivity.class);
+                intent.putExtra("mobile", "+91" + mobile);
+                Log.d("mobile no", "+91" + mobile);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
